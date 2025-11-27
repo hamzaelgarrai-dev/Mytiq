2aef8be7990d150608de0c3049b685be13086fa0
“[Hamza <[hamzaelgarrai.dev@gmail.com]>
2025-11-27 11:50:15 +0100

    adding request validation for event and admin middelwere

--- backend/app/Models/Event.php
+++ backend/app/Models/Event.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title' , 'description','category', 'event_date', 'location', 'price', 'capacity', 'status' , 'aviliable_tickets', 'image_url'];
+
+        public function tickets(){
+        
+        return $this->hasMany(Ticket::class);
+    }
 }
+
+
