a85e9535d17171bf9c3007846eadc4403933e974
wijdane <wijdanesalik06@gamil.com>
2025-11-27 14:39:26 +0100

    controller newsletter

--- backend/app/Models/NewsLetter.php
+++ backend/app/Models/NewsLetter.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
-class NewsLetter extends Model
+class Newsletter extends Model
 {
     use HasFactory;
+
+    
+    protected $table = 'news_letters';
+
+    
+    protected $fillable = [
+        'email'
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'email', 'email');
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\TicketController;
+use App\Http\Controllers\Api\NewsletterController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+// Newsletter subscription (public)
+Route::post('/newsletter/subscribe', [NewsletterController::class, 'subscribe']);
+
+// Newsletter subscribers management (admin only)
+Route::get('/newsletter/subscribers', [NewsletterController::class, 'index']);
+Route::get('/newsletter/subscribers/{id}', [NewsletterController::class, 'show']);
+Route::delete('/newsletter/subscribers/{id}', [NewsletterController::class, 'destroy']);
